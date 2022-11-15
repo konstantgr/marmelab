8b0d115386f9d456a5e85ffb8d85661f6e0a57e6
Vladimir <v.igoshin01@mail.ru>
2022-11-15 22:24:54 +0300

    QSmartTable MVC

--- pyqt_app/CentralWidgets/QSmartTable.py
+++ pyqt_app/CentralWidgets/QSmartTable.py
@@ -1,8 +1,10 @@
-from PyQt6.QtWidgets import QTableWidget, QWidget, QItemDelegate, QLineEdit, QTableWidgetItem, QSizePolicy
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QTableWidget, QWidget, QItemDelegate, QLineEdit, QTableWidgetItem, QSizePolicy, QTableView, QStyledItemDelegate, QStyleOptionViewItem, QHeaderView
+from PyQt6.QtCore import Qt, QAbstractTableModel
+from PyQt6 import QtCore
 from PyQt6.QtGui import QColor, QValidator
+from PyQt6 import QtGui, QtWidgets
 from dataclasses import dataclass
-from typing import Union, Type
+from typing import Union, Type, Any
 import re
 from PyQt6.QtCore import pyqtSignal, pyqtBoundSignal, QObject
 
@@ -89,83 +91,158 @@ class NoUnit(Unit):
 
 
 @dataclass
-class Setting:
-    var_name: str
-    units: Type[Unit]
-    default_value: float
-    description: str
-    type: Union[Type[int], Type[float], Type[bool]]
+class Variable:
+    """
+    Класс переменной для таблицы
+    """
+    name: str
+    default_value: Union[int, float]
+    type: Union[Type[int], Type[float]]
+    unit: Type[Unit] = NoUnit
+    description: str = ''
 
 
-class QSmartTableItem(QTableWidgetItem):
-    def __init__(self, text: str, setting: Setting):
-        super(QSmartTableItem, self).__init__(text)
-        self.setting = setting
-        self.valid = True
+class QSmartTableModel(QAbstractTableModel):
+    """
+    Реализация Модели
+    """
+    def __init__(self, variables: list[Variable], parent: QObject = None):
+        super().__init__(parent)
+        self.variables = variables
+        self._data = [str(var.default_value) for var in variables]
+        self.headers = ['Variable', 'Value', 'Unit', 'Description']
+        self.pattern = re.compile(r"^\d+(?:\.\d+)?\s*(?:\[\s*[a-zA-Z]*\s*])?$")
+
+    def rowCount(self, parent: QtCore.QModelIndex = ...) -> int:
+        return len(self._data)
+
+    def columnCount(self, parent: QtCore.QModelIndex = ...) -> int:
+        return len(self.headers)
+
+    def data(self, index: QtCore.QModelIndex, role: int = ...) -> Any:
+        row = index.row()
+        column = index.column()
+        variable = self.variables[row]
+        if role == Qt.ItemDataRole.DisplayRole or role == Qt.ItemDataRole.EditRole:
+            if column == 0:
+                return variable.name
+            elif column == 1:
+                return self._data[row]
+            elif column == 2:
+                if self._valid(self._data[row], variable):
+                    _, unit = self._value_str_and_unit(self._data[row], variable)
+                    return unit
+                return variable.unit.default_unit()
+            elif column == 3:
+                return variable.description
+        if role == Qt.ItemDataRole.ToolTipRole:
+            if column == 3:
+                return variable.description
+        elif role == Qt.ItemDataRole.BackgroundRole:
+                if self._valid(self._data[row], variable):
+                    return
+                return QColor('red')
+
+    @staticmethod
+    def _value_str_and_unit(text: str, variable: Variable):
+        """
+        Принимает текст " 123.2 [ xy ] ", преобразует его в ("123.2", "xy").
+        В случае " 123.2", преобразует текст в ("123.2", "xy"), где "xy" -- дефолтные единицы измерения переменной.
+
+        :param text:
+        :param variable:
+        :return:
+        """
+        text = text.strip()
+        if text.count('['):
+            value, unit = text[:-1].split('[')
+            return value.strip(), unit.strip()
+        else:
+            return text.strip(), variable.unit.default_unit()
+
+    def _valid(self, value: str, variable: Variable):
+        """
+        Проверяет валидность введенного текста
+
+        :param value:
+        :param variable:
+        :return:
+        """
+        text = value.strip()
+        if not re.match(self.pattern, text):
+            return False
+
+        value_str, unit = self._value_str_and_unit(text, variable)
+
+        if not variable.unit.validate(unit):
+            return False
+
+        try:
+            variable.type(value_str)
+        except:
+            return False
+
+        return True
+
+    def flags(self, index: QtCore.QModelIndex) -> Qt.ItemFlag:
+        flags = Qt.ItemFlag.ItemIsSelectable | Qt.ItemFlag.ItemIsEnabled
+        if index.column() == 1:
+            flags |= Qt.ItemFlag.ItemIsEditable
+        return flags
+
+    def setData(self, index: QtCore.QModelIndex, value: Any, role: int = ...) -> bool:
+        if role == Qt.ItemDataRole.EditRole:
+            row = index.row()
+            self._data[row] = value
+
+            for i in range(len(self.headers)):
+                unit_cell_index = index.sibling(0, i-1)
+                self.dataChanged.emit(unit_cell_index, unit_cell_index)
+        return True
+
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int = ...) -> Any:
+        if role == Qt.ItemDataRole.DisplayRole:
+            if orientation == Qt.Orientation.Horizontal:
+                return self.headers[section]
+
+    def to_dict(self):
+        res = {}
+        for i in range(len(self.variables)):
+            text = self._data[i]
+            variable = self.variables[i]
+            try:
+                value_str, unit = self._value_str_and_unit(text, variable)
+                value = variable.unit.to_default_unit(variable.type(value_str), unit)
+                res[variable.name] = value
+            except Exception as e:
+                raise e
+        return res
+
+    def set_default(self):
+        for i, variable in enumerate(self.variables):
+            index = self.index(i, 1)
+            self.setData(index, str(variable.default_value), Qt.ItemDataRole.EditRole)
 
 
-class QSmartTable(QTableWidget):
+class QSmartTable(QTableView):
     """
     Реализация умной таблицы
     """
 
-    acceptable_table: pyqtBoundSignal = pyqtSignal(bool)
+    valid: pyqtBoundSignal = pyqtSignal(bool)
 
-    def __init__(self, settings: list[Setting], parent: QWidget = None):
-        super(QSmartTable, self).__init__(len(settings), 4, parent)
-        self.settings = settings
-        self.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Maximum))
-        self.value_items = []
+    def __init__(self, variables: list[Variable], parent: QWidget = None):
+        super(QSmartTable, self).__init__(parent)
+        self.setModel(QSmartTableModel(variables))
 
-        for i, setting in enumerate(self.settings):
-            name_item = QTableWidgetItem(setting.var_name)
-            name_item.setFlags(Qt.ItemFlag.ItemIsEditable)
-            name_item.setForeground(QColor('gray'))
-            self.setItem(i, 0, name_item)
+        header = self.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.ResizeMode.Interactive)
+        header.setStretchLastSection(True)
 
-            value_item = QSmartTableItem(str(setting.default_value), setting)
-            self.value_items.append(value_item)
-            self.setItem(i, 1, value_item)
+    def to_dict(self):
+        model: QSmartTableModel = self.model()
+        return model.to_dict()
 
-            units_item = QTableWidgetItem(setting.units.default_unit())
-            units_item.setFlags(Qt.ItemFlag.ItemIsEditable)
-            units_item.setForeground(QColor('gray'))
-            self.setItem(i, 2, units_item)
-
-            description_item = QTableWidgetItem(setting.description)
-            description_item.setFlags(Qt.ItemFlag.ItemIsEditable)
-            description_item.setForeground(QColor('gray'))
-            self.setItem(i, 3, description_item)
-
-        self.setHorizontalHeaderLabels(['name', 'value', 'units', 'description'])
-        self.itemChanged.connect(self.checkData)
-
-        self.pattern = re.compile(r"^\d+(?:\.\d+)?(?:\[[a-zA-Z]*\])?$")
-
-    def checkData(self, item: QSmartTableItem):
-        text = item.text().strip()
-        print(item.text())
-        if not re.match(self.pattern, text):
-            item.setBackground(QColor('red'))
-            item.valid = False
-            self.acceptable_table.emit(False)
-            return False
-
-        if text.count('['):
-            value_str, units = text[:-1].split('[')
-            value = item.setting.type(value_str)
-        else:
-            units = item.setting.units.default_unit()
-            value = item.setting.type(text)
-
-        if not item.setting.units.validate(units):
-            item.setBackground(QColor('red'))
-            item.valid = False
-            self.acceptable_table.emit(False)
-            return False
-
-        item.setBackground(QColor('white'))
-        item.valid = True
-        self.item(item.row(), item.column()+1).setText(units)
-        self.acceptable_table.emit(all([x.valid for x in self.value_items]))
-        return True
+    def set_default(self):
+        model: QSmartTableModel = self.model()
+        model.set_default()
--- pyqt_app/CentralWidgets/RoomSettings.py
+++ pyqt_app/CentralWidgets/RoomSettings.py
@@ -1,15 +1,41 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QSizePolicy
-from .QSmartTable import QSmartTable, Setting, Length
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QSizePolicy, QHBoxLayout
+from .QSmartTable import QSmartTable, Variable, Length, NoUnit, Time, Frequency
+from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
 
 
 class RoomSettings(QWidget):
     def __init__(self):
         super(RoomSettings, self).__init__()
-        layout = QVBoxLayout()
+        self.layout = QVBoxLayout()
+        self.layout.setAlignment(Qt.AlignmentFlag.AlignTop)
+        self.setLayout(self.layout)
+
         settings = [
-            Setting('test', Length, 1, 'kek', float),
-            Setting('test2', Length, 1, 'kek1', float)
+            Variable(name='test', unit=NoUnit, default_value=1, description='kek', type=float),
+            Variable(name='test2', unit=Length, default_value=2, description='kek12394 324 j234k 23j4lk2j 4j2k l4j2j 2lk3 j42lk3j 4lk23j4 k2j4 3', type=float)
         ]
-        table = QSmartTable(settings)
-        layout.addWidget(table)
-        self.setLayout(layout)
+        self.table = QSmartTable(settings)
+        self.table.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Maximum)
+        self.layout.addWidget(self.table)
+
+        buttons_layout = QHBoxLayout()
+        buttons_layout.setAlignment(Qt.AlignmentFlag.AlignRight)
+
+        set_default_button = QtWidgets.QPushButton("Set default")
+        set_default_button.clicked.connect(self.table.set_default)
+        set_default_button.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        buttons_layout.addWidget(set_default_button)
+
+        apply_button = QtWidgets.QPushButton("Apply")
+        apply_button.clicked.connect(self.apply)
+        apply_button.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
+        buttons_layout.addWidget(apply_button)
+        buttons_layout.setStretch(0, 1)
+        buttons_layout.setStretch(1, 1)
+
+        self.layout.addLayout(buttons_layout)
+
+
+    def apply(self):
+        print(self.table.to_dict())
