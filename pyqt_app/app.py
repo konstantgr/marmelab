db409c2b45fa91be33b46b926c40c09539da7ead
Sergej Koromyslov <sergej.koromyslov@metalab.ifmo.ru>
2022-12-14 15:34:23 +0300

    tool panel

--- pyqt_app/app.py
+++ pyqt_app/app.py
@@ -4,7 +4,7 @@ from pyqt_app import app
 
 if __name__ == "__main__":
     window = MainWindow()
-    tool_bar = window.addToolBar("s")
+    #tool_bar = window.addToolBar("s")
     #tool_bar.addAction(icon=unnamed.png)
     window.setGeometry(300, 300, 900, 600)
     window.show()
