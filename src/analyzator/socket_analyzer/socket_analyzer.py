51431561d9203674590dfe9c978982b88f4e0296
Vsevolod Kleshchenko <k.vd@icloud.com>
2022-12-26 18:21:27 +0300

    correct definition of traces (with window); correct power settings; (26.12 in bek)

--- src/analyzator/socket_analyzer/socket_analyzer.py
+++ src/analyzator/socket_analyzer/socket_analyzer.py
@@ -43,14 +43,11 @@ class SocketAnalyzer(BaseAnalyzer):
 
     def _send_cmd(self, cmd: str):
         self.instrument.sendall(str.encode(cmd+'\n'))
-        print(cmd)
         time.sleep(0.1)
         if '?' in cmd:
-            # return self.instrument.recv(1024).decode()
             response = bytearray()
             while not response.endswith(b'\n'):
                 chunk = self.instrument.recv(self.bufsize)
-                print(chunk)
                 if not chunk:
                     break
                 response += chunk
@@ -64,8 +61,10 @@ class SocketAnalyzer(BaseAnalyzer):
                      freq_num: int = None,
                      bandwidth: float = None,
                      aver_fact: int = None,
-                     smooth_aper: int = None
+                     smooth_aper: int = None,
+                     power: int = None
                      ) -> None:
+
         self._send_cmd("*RST")
         self.channel = channel
         if sweep_type is not None:
@@ -84,6 +83,10 @@ class SocketAnalyzer(BaseAnalyzer):
         if smooth_aper is not None:
             self._send_cmd(f'CALC{channel}:SMO:STAT ON')
             self._send_cmd(f'CALC{channel}:SMO:APER {smooth_aper}')
+        if power is not None:
+            number_of_ports = int(self._send_cmd(f'SERV:PORT:COUN?'))
+            for n_port in range(1, number_of_ports+1):
+                self._send_cmd(f'SOUR{channel}:POW{n_port} {power}dBm')
 
     def _set_is_connected(self, state: bool):
         self._is_connected = state
@@ -115,18 +118,21 @@ class SocketAnalyzer(BaseAnalyzer):
 
         if not self.is_connected:
             raise AnalyzerConnectionError
-        # self._send_cmd(f"CALC{self.channel}:PAR:COUN 8")
+
+        freq_data = self._send_cmd(f'SENS{self.channel}:FREQ:DATA?')
+
         for num, s_param in enumerate(parameters):
             num += 1
-            # self._send_cmd(f"CALC{self.channel}:PAR:COUN {num}")
-            self._send_cmd(f"CALC{self.channel}:PAR:DEF 'Tr {num}',{s_param}")
-            self._send_cmd(f"CALC{self.channel}:PAR:SEL 'Tr {num}'")
+            self._send_cmd(f"CALC{self.channel}:PAR:DEF 'Tr{num}',{s_param}")
+            self._send_cmd(f"DISPlay:WINDow1:TRACe2:FEED 'Tr{num}'")
+            # print(self._send_cmd(f"CALC{self.channel}:PAR:CAT?"))
+            self._send_cmd(f"CALC{self.channel}:PAR:SEL 'Tr{num}'")
             trace_data = self._send_cmd(f'CALC{self.channel}:DATA? SDATA')
             trace_tup = tuple(map(str, trace_data.split(',')))
             trace_array = np.array(trace_tup).astype(float)
             res[f'{s_param}'] = trace_array[:-1:2] + 1j * trace_array[1::2]
+            self._send_cmd(f"CALC{self.channel}:PAR:DEL 'Tr{num}'")
 
-        freq_data = self._send_cmd(f'SENS{self.channel}:FREQ:DATA?')  # rsinstrument examples csv
         freq_tup = tuple(map(str, freq_data.split(',')))
         res[f'f'] = np.array(freq_tup).astype(float)
 
@@ -144,11 +150,9 @@ class SocketAnalyzer(BaseAnalyzer):
 
 
 if __name__ == "__main__":
-    analyzer = SocketAnalyzer(ip="192.168.137.245", port=1024)
+    analyzer = SocketAnalyzer(ip="192.168.137.119", port=1024)
     analyzer.connect()
-    print('connect')
     analyzer.set_settings(sweep_type='LIN', freq_start=1000000000, freq_stop=3000000000,
-                          freq_num=200, bandwidth=3000, aver_fact=5, smooth_aper=20)
-    print('settings done')
-    results = analyzer.get_scattering_parameters(['S22'])
-    print(results['f'], results['S11'])
+                          freq_num=200, bandwidth=3000, aver_fact=5, smooth_aper=20, power=5)
+    results = analyzer.get_scattering_parameters(['S22', 'S12', 'S12', 'S12', 'S12', 'S12', 'S12', 'S12', 'S12', 'S12'])
+    print(results['f'], results['S22'])
